69d357e1a82b15680a7618bde687f17681801f02
TheShuai <1019818712@qq.com>
2023-03-01 21:20:27 +0800

    1.0基础布局

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
   <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+    <router-view />
   </div>
 </template>
-
+ 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  lintOnSave: false,
+  transpileDependencies: true,
+  devServer: {
+    open: false,//自动打开浏览器
+    port: 9000,//配置项目启动的规定端口号
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8090',
+        ws: true,//用于支持websocket
+        //用于控制请求头中host值（端口，判断是否说谎，默认值是true）
+        changeOrigin: true,//true:5001  false:9000
+        pathRewrite: { '^/api': "" }//重写路径，也就是把/api前缀给替换为空格
+      }
+    }
+  }
 })
